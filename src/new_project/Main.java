e8954021f48aad55336549eb1678e42dc532ad33
Maxfiybek <mirjamolq828@gmail.com>
2024-02-13 12:17:51 +0500

    new_Project

--- src/new_project/Main.java
+++ src/new_project/Main.java
@@ -3,5 +3,6 @@ package new_project;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Android !");
+        System.out.println("Updated git hub !");
     }
 }
